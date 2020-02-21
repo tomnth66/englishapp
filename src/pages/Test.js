420892756da2d2ec1e79e9e6dbe24e1ad68b55ad
newbee1905 <beenewminh@outlook.com>
2020-02-21 12:17:38 +0700

    add material ui

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,10 +1,58 @@
 // this file is use for example component
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar.js";
-import InputForm from "../components/InputForm.js";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle
+} from "@material-ui/core";
+import {
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Input
+} from "@material-ui/core";
+import { Zoom, Slide } from "@material-ui/core";
 import "flexboxgrid";
 
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 240
+  }
+}));
+
 const Test = () => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [type, setType] = useState("");
+  const [name, setName] = useState("");
+
+  const handleChangeType = event => {
+    setType(event.target.value || "");
+  };
+
+  const handleChangeName = event => {
+    setName(event.target.value || "");
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <div style={{ height: "100vh" }}>
       <Navbar />
@@ -12,7 +60,60 @@ const Test = () => {
         className="row middle-xs center-xs"
         style={{ height: "calc(100% - 4rem)" }}
       >
-        <InputForm label="Test" className="col-xs-10" />
+        <Button onClick={handleClickOpen}>Open select dialog</Button>
+        <Dialog
+          disableBackdropClick
+          disableEscapeKeyDown
+          open={open}
+          onClose={handleClose}
+        >
+          <DialogTitle>Fill the form</DialogTitle>
+          <DialogContent>
+            <form className={classes.container}>
+              <FormControl className={classes.formControl} required>
+                <InputLabel id="demo-dialog-select-label">Type</InputLabel>
+                <Select
+                  labelId="demo-dialog-select-label"
+                  id="demo-dialog-select"
+                  value={type}
+                  onChange={handleChangeType}
+                  input={<Input />}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl className={classes.formControl} required>
+                <InputLabel htmlFor="my-input">Email address</InputLabel>
+                <Input
+                  id="my-input"
+                  aria-describedby="my-helper-text"
+                  value={name}
+                  onChange={handleChangeName}
+                />
+                {/* <FormHelperText id="my-helper-text">
+                  We'll never share your email.
+                </FormHelperText> */}
+              </FormControl>
+            </form>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button
+              onClick={handleClose}
+              color="primary"
+              disabled={!type || !name}
+            > 
+              Ok
+            </Button>
+          </DialogActions>
+        </Dialog>
       </div>
     </div>
   );
