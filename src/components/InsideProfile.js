2ce1af2c5416187c1461be936924930cedda4aea
newbee1905 <beenewminh@outlook.com>
2020-03-12 19:19:35 +0700

    add css to anouncement

--- src/components/InsideProfile.js
+++ src/components/InsideProfile.js
@@ -1,37 +1,56 @@
-import React, { Component } from "react";
-import "../css/InsideProfile.css";
-import Avatar from "./Avatar.js";
-import Button from './Button.js';
+import React, { Component } from 'react';
+import '../css/InsideProfile.css';
+import Avatar from './Avatar.js';
+// import Button from './Button.js';
+import { Button } from '@material-ui/core';
 
-export default class InsideProfile extends Component {
-  constructor(props) {
-    super(props);
-  }
+class InsideProfile extends Component {
+	constructor(props) {
+		super(props);
+	}
 
-  handleLogOut(){
-    console.log('logging out')
-    localStorage.clear();
-    window.location.href="/";
-  }
+	handleLogOut() {
+		console.log('logging out');
+		localStorage.clear();
+		window.location.href = '/';
+	}
 
-  render() {
-    return (
-      <div className="InsideProfile">
-        <Avatar></Avatar>
-        {/* <Button></Button> */}
-        <div className="Information">
-          <p>Name: {localStorage.getItem('user')} ({localStorage.getItem('class')})</p>
+	render() {
+		return (
+			<div className="InsideProfile">
+				<Avatar></Avatar>
+				{/* <Button></Button> */}
+				<div className="Information">
+					<p>
+						Name: {localStorage.getItem('user')} (
+						{localStorage.getItem('class')})
+					</p>
 
-          <div className = 'Action'>
-            <a href = {'../Profile/'+localStorage.getItem('userId')}> Profile</a>
-            {localStorage.getItem('class')==='admin' && <a href = '/createmap'> Post an announcement</a>}
-            {localStorage.getItem('class')==='admin' && <a href = '/createmap'> Createmap</a>}
-            {localStorage.getItem('class')==='admin' && <a href = '/studentmanagement'> Student Management</a>}
-          </div>
-          
-          <Button keyword = 'Logout' onClick = {this.handleLogOut.bind(this)}></Button>
-        </div>
-      </div>
-    );
-  }
+					<div className="Action">
+						<a href={'../Profile/' + localStorage.getItem('userId')}>
+							{' '}
+							Profile
+						</a>
+						{localStorage.getItem('class') === 'admin' && (
+							<a href="/createmap"> Post an announcement</a>
+						)}
+						{localStorage.getItem('class') === 'admin' && (
+							<a href="/createmap"> Createmap</a>
+						)}
+						{localStorage.getItem('class') === 'admin' && (
+							<a href="/studentmanagement"> Student Management</a>
+						)}
+					</div>
+
+					<Button
+						variant="contained"
+						color="primary"
+						onClick={this.handleLogOut.bind(this)}
+					>Log Out</Button>
+				</div>
+			</div>
+		);
+	}
 }
+
+export default InsideProfile;
