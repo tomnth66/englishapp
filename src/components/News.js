ace2473e9efe9d132792b8ea4e695eda1a35bdb9
tomnth66 <51469580+tomnth66@users.noreply.github.com>
2020-03-16 21:36:28 +0700

    fix delete Ann
    fix delete Ann

--- src/components/News.js
+++ src/components/News.js
@@ -16,27 +16,20 @@ export default class News extends Component {
   }
 
   GetDB(){
-    return new Promise((resolve, reject) => {
-      const db = firebase.firestore();
+    const db = firebase.firestore();
       const ref = db.collection("Announcement").doc("A4TT0kGAgjKUxs2wrj8p");
       ref.get().then(data => {
         this.setState({
           AnnouncementList: data.data().AnnouncementList
-        },()=>resolve('done'));
+        });
       });
-    }) 
   }
 
 
 
-  // GetDB() {
-    
-  // }
-
-  
-
   render() {
     const {AnnouncementList} = this.state;
+    
     return(
       <div className="News">
         <h1>Announcement</h1>
