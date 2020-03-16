06b2a10390bd12bb11cd2cd89f108da3125d241d
newbee1905 <beenewminh@outlook.com>
2020-03-17 00:58:27 +0700

    change edit/delete button in announcement

--- src/components/InsideNews.js
+++ src/components/InsideNews.js
@@ -1,24 +1,24 @@
-import React, { Component } from "react";
-import "../css/InsideNews.css";
-import Newsmain from './Newsmain.js'
-import Newsfooter from './Newsfooter.js'
-
+import React, { Component } from 'react';
+import '../css/InsideNews.css';
+import Newsmain from './Newsmain.js';
+import Newsfooter from './Newsfooter.js';
 
 export default class InsideNews extends Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
- 
+	render() {
+		const { Announcement } = this.props;
+		return (
+			<div className="InsideNews">
+				<Newsmain
+					Announcement={Announcement}
+					GetDB={this.props.GetDB}
+				></Newsmain>
 
-  render() {
-    const { Announcement } = this.props;
-    return (
-      <div className = 'InsideNews'>
-        <Newsmain Announcement = {Announcement} GetDB = {this.props.GetDB}></Newsmain>
-
-        <Newsfooter Announcement={Announcement}></Newsfooter>
-      </div>
-    );
-  }
+				<Newsfooter Announcement={Announcement}></Newsfooter>
+			</div>
+		);
+	}
 }
--- src/components/Newsmain.js
+++ src/components/Newsmain.js
@@ -1,109 +1,62 @@
-import React, { Component } from "react";
-import "../css/InsideNews.css";
-import firebase from '../firebase.js'
-import Ava from '../img/stitch.jpg'
+import React, { Component } from 'react';
+import '../css/InsideNews.css';
+import firebase from '../firebase.js';
+import Ava from '../img/stitch.jpg';
 import 'font-awesome/css/font-awesome.min.css';
-import { Button } from '@material-ui/core';
-import Loading from "./Loading.js";
+import { Button, Card, Tab, IconButton } from '@material-ui/core';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import Loading from './Loading.js';
 
 export default class Newsmain extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isShowSetting: false,
-      EditState:false,
-      originValue:'',
-      value:'',
-      content: '',
-      isLoading: false
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			isShowSetting: false,
+			EditState: false,
+			originValue: '',
+			value: '',
+			content: '',
+			isLoading: false
+		};
 
-    this.inputElement1 = React.createRef();
-    this.inputElement2 = React.createRef();
-  }
+		this.inputElement1 = React.createRef();
+		this.inputElement2 = React.createRef();
+	}
 
-  componentDidMount(){
-    this.setState({
-      isShowSetting: false,
-      EditState:false,
-      originValue:this.props.Announcement.AnnouncementContent,
-      value:this.props.Announcement.AnnouncementContent,
-      isLoading: false
-    },()=>this.updateRenderDB())
-  }
+	componentDidMount() {
+		this.setState(
+			{
+				isShowSetting: false,
+				EditState: false,
+				originValue: this.props.Announcement.AnnouncementContent,
+				value: this.props.Announcement.AnnouncementContent,
+				isLoading: false
+			},
+			() => this.updateRenderDB()
+		);
+	}
 
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.Announcement !== this.props.Announcement) {
+			this.setState(
+				{
+					isShowSetting: false,
+					EditState: false,
+					originValue: this.props.Announcement.AnnouncementContent,
+					value: this.props.Announcement.AnnouncementContent,
+					isLoading: false
+				},
+				() => this.updateRenderDB()
+			);
+		}
+	}
 
-  componentWillReceiveProps(nextProps){
-    if(nextProps.Announcement !== this.props.Announcement){
-      this.setState({
-        isShowSetting: false,
-        EditState:false,
-        originValue:this.props.Announcement.AnnouncementContent,
-        value:this.props.Announcement.AnnouncementContent,
-        isLoading: false
-      },()=>this.updateRenderDB())
-    }
-  }
-
-  updateRenderDB(){
-    let curAnn = this.props.Announcement.AnnouncementContent;
-    let words = curAnn.split('\n');
-    let fullPara = '';
-    
-    fullPara+=`#${this.props.Announcement.id}\n`;
-
-		for (let word of words) {
-			fullPara += `<div>\n`;
-			word = word.split(' ');
-			for (let i = 0; i < word.length; ++i) {
-				if (word[i] !== '') {
-					fullPara += `${word[i]} `;
-        } 
-        else {
-					fullPara += '<div style="height: 1rem"></div>';
-				}
-			}
-			fullPara += `</div>\n`;
-    }
-    
-    this.setState({
-      content:fullPara
-    })
-  }
- 
-
-  showSetting(){
-    this.setState({
-      isShowSetting:!this.state.isShowSetting
-    }, ()=>{document.addEventListener('mousedown',this.handleClick.bind(this),false);})
-  }
-
-
-
-  handleClick(event){
-    if(this.state.isShowSetting){
-      if(!this.inputElement1.current.contains(event.target) 
-      && !this.inputElement2.current.contains(event.target)){
-          this.setState({
-            isShowSetting: false,
-          }) 
-      }
-    }
-  }
-
-
-
-  saveEdit(){
-    this.setState({
-      isLoading:true
-    })
-    const db = firebase.firestore();
-    const ref = db.collection("Announcement").doc("A4TT0kGAgjKUxs2wrj8p");
-    let curAnn = document.getElementById('EditTextArea').value;
-    let words = curAnn.split('\n');
+	updateRenderDB() {
+		let curAnn = this.props.Announcement.AnnouncementContent;
+		let words = curAnn.split('\n');
 		let fullPara = '';
 
-    fullPara+=`#${this.props.Announcement.id}\n`;
+		fullPara += `#${this.props.Announcement.id}\n`;
 
 		for (let word of words) {
 			fullPara += `<div>\n`;
@@ -111,148 +64,219 @@ export default class Newsmain extends Component {
 			for (let i = 0; i < word.length; ++i) {
 				if (word[i] !== '') {
 					fullPara += `${word[i]} `;
-        } 
-        else {
+				} else {
 					fullPara += '<div style="height: 1rem"></div>';
 				}
 			}
 			fullPara += `</div>\n`;
-    }
+		}
 
-    ref.get().then(data => {
-      let AnnouncementList = data.data().AnnouncementList;
-      let idx = AnnouncementList.findIndex(Ann => Ann.id === this.props.Announcement.id);
+		this.setState({
+			content: fullPara
+		});
+	}
 
-      AnnouncementList[idx].AnnouncementContent = curAnn;
+	showSetting() {
+		this.setState(
+			{
+				isShowSetting: !this.state.isShowSetting
+			},
+			() => {
+				document.addEventListener(
+					'mousedown',
+					this.handleClick.bind(this),
+					false
+				);
+			}
+		);
+	}
 
-      ref.set({ AnnouncementList: AnnouncementList })
-         .then(()=>{
-                    this.setState({
-                      EditState:false,
-                      content:fullPara,
-                      isLoading: false,
-                      originValue:this.state.value
-                    })
-                   });
-    });
-  }
+	handleClick(event) {
+		if (this.state.isShowSetting) {
+			if (
+				!this.inputElement1.current.contains(event.target) &&
+				!this.inputElement2.current.contains(event.target)
+			) {
+				this.setState({
+					isShowSetting: false
+				});
+			}
+		}
+	}
 
+	saveEdit() {
+		this.setState({
+			isLoading: true
+		});
+		const db = firebase.firestore();
+		const ref = db.collection('Announcement').doc('A4TT0kGAgjKUxs2wrj8p');
+		let curAnn = document.getElementById('EditTextArea').value;
+		let words = curAnn.split('\n');
+		let fullPara = '';
 
+		fullPara += `#${this.props.Announcement.id}\n`;
 
-  handleTextChange(e){
-    this.setState({
-      value: e.target.value
-    });
-  }
+		for (let word of words) {
+			fullPara += `<div>\n`;
+			word = word.split(' ');
+			for (let i = 0; i < word.length; ++i) {
+				if (word[i] !== '') {
+					fullPara += `${word[i]} `;
+				} else {
+					fullPara += '<div style="height: 1rem"></div>';
+				}
+			}
+			fullPara += `</div>\n`;
+		}
 
+		ref.get().then(data => {
+			let AnnouncementList = data.data().AnnouncementList;
+			let idx = AnnouncementList.findIndex(
+				Ann => Ann.id === this.props.Announcement.id
+			);
 
+			AnnouncementList[idx].AnnouncementContent = curAnn;
 
+			ref.set({ AnnouncementList: AnnouncementList }).then(() => {
+				this.setState({
+					EditState: false,
+					content: fullPara,
+					isLoading: false,
+					originValue: this.state.value
+				});
+			});
+		});
+	}
 
-  EditNews(){
-    this.setState({
-      isShowSetting:false,
-      EditState:true
-    },()=>{
-      const Ref = this.EditTextRef;
-      const length = this.EditTextRef.value.length;
-      Ref.focus();
-      Ref.setSelectionRange(length, length);
-    })
-  }
+	handleTextChange(e) {
+		this.setState({
+			value: e.target.value
+		});
+	}
 
+	EditNews() {
+		this.setState(
+			{
+				isShowSetting: false,
+				EditState: true
+			},
+			() => {
+				const Ref = this.EditTextRef;
+				const length = this.EditTextRef.value.length;
+				Ref.focus();
+				Ref.setSelectionRange(length, length);
+			}
+		);
+	}
 
+	DeleteNews() {
+		this.setState({
+			isLoading: true
+		});
 
+		const db = firebase.firestore();
+		const ref = db.collection('Announcement').doc('A4TT0kGAgjKUxs2wrj8p');
 
+		ref.get().then(data => {
+			let AnnouncementList = data.data().AnnouncementList;
+			let idx = AnnouncementList.findIndex(
+				Ann => Ann.id === this.props.Announcement.id
+			);
 
-  DeleteNews(){
-    this.setState({
-      isLoading:true,
-    })
+			AnnouncementList.splice(idx, 1);
 
-    const db = firebase.firestore();
-    const ref = db.collection("Announcement").doc("A4TT0kGAgjKUxs2wrj8p");
+			ref.set({ AnnouncementList: AnnouncementList }).then(() => {
+				this.setState(
+					{
+						isShowSetting: false,
+						isLoading: false
+					},
+					() => this.props.GetDB()
+				);
+			});
+		});
+	}
 
-    ref.get().then(data => {
-      let AnnouncementList = data.data().AnnouncementList;
-      let idx = AnnouncementList.findIndex(Ann => Ann.id === this.props.Announcement.id );
-      
-      AnnouncementList.splice(idx, 1);
+	closeEdit() {
+		this.setState({
+			EditState: false,
+			value: this.state.originValue
+		});
+	}
 
-      ref.set({ AnnouncementList: AnnouncementList })
-         .then( ()=>{
-                      this.setState({
-                        isShowSetting:false,
-                        isLoading: false
-                      }, ()=>this.props.GetDB())
-                    })
-    });
-  }
+	render() {
+		return (
+			<div className="Newsmain">
+				{this.state.isLoading && <Loading />}
+				<div className="Auther">
+					<div className="AutherAvatar">
+						<img className="Picture" src={Ava}></img>
+					</div>
+					<div className="AutherName">
+						<span>San</span>
+					</div>
 
+					{localStorage.getItem('class') === 'admin' && (
+						<div className="News-icons">
+							<IconButton
+								ref={this.inputElement1}
+								onClick={this.showSetting.bind(this)}
+							>
+                <MoreHorizIcon id="curIcon"/>
+							</IconButton>
+						</div>
+					)}
 
+					{this.state.isShowSetting && (
+						<Card ref={this.inputElement2} className="News-setting">
+							<Tab
+								onClick={this.EditNews.bind(this)}
+								label="Edit"
+								style={{ minWidth: '7rem' }}
+							/>
+							<Tab
+								onClick={this.DeleteNews.bind(this)}
+								label="Delete"
+								style={{ minWidth: '7rem' }}
+							/>
+						</Card>
+					)}
+				</div>
 
+				<div className="NewsContent">
+					{!this.state.EditState && (
+						<div dangerouslySetInnerHTML={{ __html: this.state.content }}></div>
+					)}
 
-  closeEdit(){
-    this.setState({
-      EditState:false,
-      value: this.state.originValue
-    })
-  }
+					{this.state.EditState && (
+						<div>
+							<textarea
+								ref={ref => (this.EditTextRef = ref)}
+								value={this.state.value}
+								onChange={this.handleTextChange.bind(this)}
+								id="EditTextArea"
+							></textarea>
+							<div className="EditNewsBtn">
+								<Button
+									color="primary"
+									variant="contained"
+									onClick={this.closeEdit.bind(this)}
+								>
+									Cancel
+								</Button>
 
-
-  
-  
-  render() {
-    return (
-      <div className = 'Newsmain'>
-        {this.state.isLoading && <Loading />}
-        <div className = 'Auther'>
-          <div className = 'AutherAvatar'>
-            <img className="Picture" src={Ava}></img>
-          </div>
-          <div className = 'AutherName'>
-            <span>San</span>
-          </div>
-          
-          {localStorage.getItem('class') === 'admin' &&
-          <div className = 'News-icons'>
-            <span ref = {this.inputElement1} 
-                  onClick = {this.showSetting.bind(this)}><i id = 'curIcon' class="fa fa-ellipsis-h"></i></span>
-          </div>}
-
-          {this.state.isShowSetting &&
-          <div ref = {this.inputElement2}  
-               className = 'News-setting'>
-              <ul>
-                <li onClick = {this.EditNews.bind(this)}>Edit</li>
-                <li onClick = {this.DeleteNews.bind(this)}>Delete</li>
-              </ul>
-          </div> 
-          }
-        </div>
-
-        <div className = 'NewsContent'>
-          {!this.state.EditState && <div dangerouslySetInnerHTML={{__html: this.state.content}}>
-                                    </div>}
-
-          {this.state.EditState && 
-            <div>
-              <textarea ref = {ref => this.EditTextRef = ref} 
-                        value = {this.state.value} 
-                        onChange = {this.handleTextChange.bind(this)}
-                        id = 'EditTextArea'></textarea> 
-              <div className = 'EditNewsBtn'>
-                <Button color="primary" variant="contained" onClick = {this.closeEdit.bind(this)}> 
-                            Cancel
-                </Button>
-
-                <Button color="primary" variant="contained" onClick = {this.saveEdit.bind(this)}> 
-                            Save
-                </Button>
-              </div>
-            </div>
-          }
-        </div>
-      </div>
-    );
-  }
+								<Button
+									color="primary"
+									variant="contained"
+									onClick={this.saveEdit.bind(this)}
+								>
+									Save
+								</Button>
+							</div>
+						</div>
+					)}
+				</div>
+			</div>
+		);
+	}
 }
