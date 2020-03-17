2e3ca039b31673143c917a63d3efedf5939c3902
tomnth66 <51469580+tomnth66@users.noreply.github.com>
2020-03-17 23:06:42 +0700

    Course Add and delete
    Course Add and delete

--- src/components/Courselist.js
+++ src/components/Courselist.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import "../css/Courselist.css";
 import firebase from "../firebase";
-import ConfirmSelectedDiv from "./ConfirmSelectedDiv.js";
-import SettingCourse from './SettingCourse.js';
+import DeleteConfirm from './DeleteConfirm.js';
+import Ids from "short-id";
 // import { Button } from '@material-ui/core';
 
 export default class Courselist extends Component {
@@ -10,9 +10,13 @@ export default class Courselist extends Component {
     super(props);
     this.state = {
       CourseList:[],
-      ConfirmSelected: false,
+      isDeleteConfirm:false,
+      isShowAddingInput:false,
+      SelectedIdx:0
       // isShowSettingCourse: false
     };
+
+    this.inputElement = React.createRef();
   }
 
   componentDidMount() {
@@ -24,18 +28,80 @@ export default class Courselist extends Component {
     const ref = db.collection("Course").doc("0zc3RakYtmCwitHgM64g");
     ref.get().then(data => {
       this.setState({
-        CourseList: data.data().CourseList
+        CourseList: data.data().CourseList,
+        isDeleteConfirm:false
       });
     });
   }
 
 
-  CloseConfirmSelected() {
-    this.setState({
-      ConfirmSelected: false
-    });
+  updateDB(event){
+    if(event.keyCode === 13){
+      let NewCourseName  = event.target.value;
+      if(!NewCourseName || NewCourseName === ''){
+        alert('Type something');
+        return;
+      }
+
+      if(NewCourseName.trim()===''){
+        alert('Type something');
+        return;
+        // hàm trim sẽ bỏ hết các dấu cách ở đầu và cuối
+      }
+
+      const db = firebase.firestore();
+      const ref = db.collection("Course").doc("0zc3RakYtmCwitHgM64g");
+
+      ref.get().then((data)=>{
+        let CourseList = data.data().CourseList;
+        
+        CourseList.unshift({
+                            Id:Ids.generate(),
+                            Name: NewCourseName
+                          })
+
+
+        ref.set({CourseList:CourseList});
+        // console.log('ref done');
+        this.setState({
+          isShowAddingInput:false
+        },()=>this.GetDB());
+      })
+    }
+
   }
 
+
+  addingCourse(){
+    this.setState({
+      isShowAddingInput:true
+    },()=>{this.inputElement.current.focus(); 
+           document.addEventListener('mousedown',this.handleClick.bind(this), false);})
+  }
+
+
+  handleClick(event){
+    if(this.state.isShowAddingInput){
+      if(!this.inputElement.current.contains(event.target)){
+        this.setState({
+          isShowAddingInput:false
+        }) 
+      }
+    }
+  }
+
+
+  closeCourseListDeleteConfirm() {
+    this.GetDB();
+  }
+
+  showCourseListDeleteConfirm(idx){
+    console.log('deleting')
+    this.setState({
+      isDeleteConfirm:true,
+      SelectedIdx:idx
+    });
+  }
   // changeStateSettingCourse(){
   //   this.setState({
   //     isShowSettingCourse:!this.state.isShowSettingCourse
@@ -44,20 +110,39 @@ export default class Courselist extends Component {
 
 
   render() {
-    const { CourseList, ConfirmSelected, SelectedId } = this.state;
+    const { CourseList, isDeleteConfirm, isShowAddingInput, SelectedId } = this.state;
     return (
       <div className="Courselist">
         <h2 className="Course--amount">
-          The numbers of the student: {CourseList.length}
+          The numbers of the course: {CourseList.length}
         </h2>
         <div className = 'CourselistTable'>
           <table>
             <tr className="Courselist--head">
               <th>STT</th>
               <th>Name</th>
-              <th></th>
-              <th></th>
+              <th colSpan = '2' className = 'AddCourseBtn' onClick = {this.addingCourse.bind(this)}>Add Course</th>
+              {/* <th></th> */}
             </tr>
+
+            {isShowAddingInput &&
+              <tr>
+                <td>0</td>
+                <td className = 'AddingInput' >
+                  <input placeholder = 'New Course'
+                         ref = {this.inputElement}
+                         onKeyUp = {(event)=>this.updateDB.bind(this)(event)} ></input>
+                </td>
+
+                <td>
+                  <span style = {{opacity:'0.5'}} className="DetailCss">Edit</span>
+                </td>
+
+                <td>
+                  <span style = {{opacity:'0.5'}} className="DetailCss">Delete</span>
+                </td>
+              </tr>
+            }
 
             {CourseList.map((course, idx) => (
             <tr>
@@ -68,11 +153,14 @@ export default class Courselist extends Component {
               </td>
 
               <td>
-                <span className="DetailCss">Delete</span>
+                <span className="DetailCss" onClick = {this.showCourseListDeleteConfirm.bind(this,idx)}>Delete</span>
               </td>
             </tr>
             ))}
           </table>
+          {isDeleteConfirm && 
+            <DeleteConfirm closeCourseListDeleteConfirm = {this.closeCourseListDeleteConfirm.bind(this)}
+                           deleteIdx = {this.state.SelectedIdx}></DeleteConfirm>}
         </div>
       </div>
     );
--- src/components/DeleteConfirm.js
+++ src/components/DeleteConfirm.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "../css/ConfirmSelectedDiv.css";
+import "../css/DeleteConfirm.css";
 import { Button } from "@material-ui/core";
 import { makeStyles, ThemeProvider } from "@material-ui/core/styles";
 import theme from "../theme/muiTheme"
@@ -8,10 +8,30 @@ import firebase from "../firebase";
 export default class DeleteConfirm extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      confirmContent:'',
+      CourseList:[]
+    }
+    this.inputElement = React.createRef();
+  }
+
+  componentDidMount(){
+    this.inputElement.current.focus(); 
+    this.GetDB();
+  }
+
+  GetDB(){
+    const db = firebase.firestore();
+    const ref = db.collection("Course").doc("0zc3RakYtmCwitHgM64g");
+    ref.get().then(data => {
+      this.setState({
+        CourseList: data.data().CourseList,
+      });
+    });
   }
 
   Unconfirmed() {
-    this.props.CloseConfirmSelected();
+    this.props.closeCourseListDeleteConfirm();
   }
 
   Confirmed() {
@@ -20,39 +40,102 @@ export default class DeleteConfirm extends Component {
     this.updateDB();
   }
 
-  updateDB() {
+
+  handleInput(event){
+    this.setState({
+      confirmContent:event.target.value
+    })
+  }
+
+  updateDB(event) {
+
+    // console.log('idxx',this.props.deleteIdx);
+
+    let confirmContent  = this.state.confirmContent;
+    if(!confirmContent || confirmContent === ''){
+      alert('Wrong Confirm');
+      return;
+    }
+
+    if(confirmContent.trim()===''){
+      alert('Wrong Confirm');
+      return;
+      // hàm trim sẽ bỏ hết các dấu cách ở đầu và cuối
+    }
+
+    if(confirmContent.trim()!=='CONFIRM'){
+      alert('Wrong Confirm');
+      return;
+    }
+
+
     // console.log(this.props.Id);
     const db = firebase.firestore();
-    const ref = db.collection("Users").doc("45GCoMKDwQWciXc8193A");
-    ref.get().then(data => {
+    const ref1 = db.collection("Course").doc("0zc3RakYtmCwitHgM64g");
+    const ref2 = db.collection("Users").doc("45GCoMKDwQWciXc8193A");
+
+
+
+
+
+    ref2.get().then(data =>{
       let Users = data.data().Users;
 
-      let idx = Users.findIndex(user => user.Id === this.props.Id);
-      
-      // console.log(Users[idx]);
-      Users.splice(idx, 1);
-      ref.set({ Users: Users }).then( ()=> window.location.href = "/studentmanagement");
-    });
+      // console.log('STAGE1F ',this.props.deleteIdx, this.state.CourseList);
+
+      let NewUsers = Users.filter(
+        user => user.Course !== this.state.CourseList[this.props.deleteIdx-1].Name
+      );
+
+      // console.log('STAGE1S ',this.props.deleteIdx, this.state.CourseList);
+
+      ref2.set({ Users: NewUsers }).then(()=>{
+        ref1.get().then(data2 => {
+
+          let CourseList = this.state.CourseList;
+    
+          // console.log('STAGE2F ',this.props.deleteIdx, this.state.CourseList);
+    
+          CourseList.splice(this.props.deleteIdx-1, 1);
+    
+          // console.log('STAGE2S ',this.props.deleteIdx, this.state.CourseList);
+          ref1.set({ CourseList: CourseList })
+          .then( ()=> this.props.closeCourseListDeleteConfirm() );
+        });
+
+
+
+
+      });
+    })
   }
+
 
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <div className="ConfirmSelectedDiv">
+        <div className="DeleteConfirm">
           <div className="Cover"></div>
 
-          <div className="ConfirmSelectedDivInside">
-            <h2 style={{margin: 0}}>{this.props.Keyword}</h2>
-            <div className="ConfirmSelectedDivButton">
-              <Button color="primary" variant="outlined" onClick={this.Unconfirmed.bind(this)}>
-                No
+          <div className="DeleteConfirmInside">
+            <h2 style={{margin: 0}}>Confirmation</h2>
+            <span className = 'DeleteConfirmWarning'>ALL STUDENTS IN THIS COURSE WILL BE DELETED Type "CONFIRM" to delete the course</span>
+            <div className = 'DeleteConfirmInput'>
+              <input placeholder = 'CONFIRM'
+                     ref = {this.inputElement}
+                     onKeyUp = {(event)=>
+                                        this.handleInput.bind(this)(event)}></input>
+            </div>
+            <div className="DeleteConfirmButton">
+              <Button style = {{marginRight:'1rem'}} color="primary" variant="outlined" onClick={this.Unconfirmed.bind(this)}>
+                Back
               </Button>
               <Button
                 color="primary"
                 variant="contained"
-                onClick={this.Confirmed.bind(this)}
+                onClick={this.updateDB.bind(this)}
               >
-                Yes
+                Delete
               </Button>
             </div>
           </div>
